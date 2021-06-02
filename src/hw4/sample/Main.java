478c2262e58c06613eee823725ffef630b463203
satan's laptop <fable777@gmail.com>
2021-06-02 22:24:18 +0300

    homeWork4

---
+++ src/hw4/sample/Main.java
@@ -0,0 +1,29 @@
+package hw4.sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.util.List;
+
+public class Main extends Application {
+
+    public static final List<String> USERS_TEST_DATA = List.of("User1", "User2", "User3");
+
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(Main.class.getResource("sample.fxml"));
+        AnchorPane root = loader.load();
+        primaryStage.setScene(new Scene(root, 600, 400));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
